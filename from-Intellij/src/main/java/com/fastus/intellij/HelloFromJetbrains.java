b6d71068e21867f24135f8a355be9363e94c7518
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-05 23:40:01 +0100

    Adding new module from Intellij

---
+++ from-Intellij/src/main/java/com/fastus/intellij/HelloFromJetbrains.java
@@ -0,0 +1,11 @@
+package com.fastus.intellij;
+
+/**
+ * Created by Tom - 05.01.2021
+ */
+public class HelloFromJetbrains {
+
+    public String sayHello(){
+        return "Hello from JetBrains!";
+    }
+}
